a9fe12935fe6da60573cad61abc53b1197e3943a
Agnus <tenebris.agnus@yandex.ru>
2018-06-17 20:01:50 +0300

    Добавила модули для работы c tsl2561 и ads1115

---
+++ Kotyarin_brain/ads1115.py
@@ -0,0 +1,202 @@
+# This program can't work with comparator.
+from i2cdev import *
+import time
+
+DEV_ADDRESS = 0x48
+# depends on ADDR sel level:
+# GND: 0x48
+# VDD: 0x49
+# SDA: 0x4A
+# SCL: 0x4B
+
+MUX = 0x04
+# 0x00: AIN0 - AIN1
+# 0x01: AIN0 - AIN3
+# 0x02: AIN1 - AIN3
+# 0x03: AIN2 - AIN3
+# 0x04: AIN0 - GND
+# 0x05: AIN1 - GND
+# 0x06:	AIN2 - GND
+# 0x07:	AIN3 - GND
+PGA = 0x00
+# 0x00:	±6,144 В
+# 0x01:	±4,096 В
+# 0x02:	±2,048 В
+# 0x03:	±1,024 В
+# 0x04:	±0,512 В
+# 0x05:	±0,256 В
+# 0x06:	±0,256 В
+# 0x07:	±0,256 В
+MODE = 0x01
+# 0x00: continuous conversion mode
+# 0x01: singl-shot mode
+DR = 0x04
+# 0x00: 8 Hz
+# 0x01: 16 Hz
+# 0x02: 32 Hz
+# 0x03: 64 Hz
+# 0x04: 128 Hz
+# 0x05: 250 Hz
+# 0x06: 475 Hz
+# 0x07: 860 Hz
+
+# Registers:
+REG_CONVERSION = 0x00
+REG_CONFIG = 0x01
+REG_MIN = 0x02
+REG_MAX = 0x03
+
+TRY_NUM = 40
+ERROR_NUM = 17
+
+
+class Ads1115_control_client ():
+    def __init__(self, i2c_, mux=MUX, pga=PGA,
+                 mode=MODE, dr=DR, address=DEV_ADDRESS):
+        self.i2c_line = i2c_
+        self.i2c_addr = address
+        self.config = [REG_CONFIG, 0x00, 0x00]
+        self.config[1] = mode | (pga << 1) | (mux << 4)
+        self.config[2] = 0x03 | (dr << 5)
+        self.k = self._count_k(PGA)
+        self.raw
+        self.time = self._count_time(dr) * 1.1
+        self.telemetry_log_vol = open('telemetry_log/vol.txt', 'a')
+        self.telemetry_log_vol.write("Time,vol_0,vol_1,vol_2,vol_3\n")
+
+    def setup(self):
+        self.i2c_line.set_addr(self.i2c_addr)
+        self.i2c_line.write(self.config)
+        if ((self.config[1] & 0x01) != 0x00):
+            self.start_conversion()
+
+    def set_mux(self, mux_=MUX):
+        self.config[1] = self.config[1]
+
+    def start_conversion(self):
+        self.i2c_line.set_addr(self.i2c_addr)
+
+        self.i2c_line.write([self.config[0],
+                             self.config[1] | 0x80,
+                             self.config[2]])
+
+    def _read_raw_voltage(self):
+        self.i2c_line.set_addr(self.i2c_addr)
+
+        self.i2c_line.write([REG_CONVERSION])
+        self.raw = self.i2c_line.read(2)
+
+    def read_voltage(self, mux):
+        self.config[1] = (self.config[1] & 0x8F) | (mux << 4)
+        self.setup()
+
+        time.sleep(self.time)
+
+        tic = 0
+        while True:
+            self.i2c_line.write(REG_CONFIG)
+            state = self.read(2)
+            if (state[0] >= 0x80):
+                break
+            if (tic >= TRY_NUM):
+                return ERROR_NUM
+            tic += 1
+
+        self._read_raw_voltage()
+
+        raw_ = (self.raw[0] << 8) + self.raw[1]
+        if (raw_ >= 32768):
+            raw_ = raw_ - 65535
+        return raw_ * c
+
+    def _count_time(self, dr):
+        if (dr == 0x00):
+            hz = 8
+        elif (dr == 0x01):
+            hz = 16
+        elif (dr == 0x02):
+            hz = 32
+        elif (dr == 0x03):
+            hz = 64
+        elif (dr == 0x04):
+            hz = 128
+        elif (dr == 0x05):
+            hz = 250
+        elif (dr == 0x06):
+            hz = 475
+        elif (dr == 0x07):
+            hz = 860
+        return 1 / hz
+
+    def _count_k(self, pga):
+        if (pga == 0x00):
+            voltage_max = 6.144
+        elif (pga == 0x01):
+            voltage_max = 4.096
+        elif (pga == 0x02):
+            voltage_max = 2.048
+        elif (pga == 0x03):
+            voltage_max = 1.024
+        elif (pga == 0x04):
+            voltage_max = 0.512
+        elif (pga >= 0x05):
+            voltage_max = 0.256
+        return voltage_max / 32768.0
+
+    def read_pins_voltage(self):
+        in_0 = ads1115.read_voltage(0x04)
+        in_1 = ads1115.read_voltage(0x05)
+        in_2 = ads1115.read_voltage(0x06)
+        in_3 = ads1115.read_voltage(0x07)
+
+        pins = [in_0, in_1, in_2, in_3]
+        self.telemetry_log_vol.write(str(time.time()) + "," +
+                                     str(pins[0]) + "," +
+                                     str(pins[1]) + "," +
+                                     str(pins[2]) + "," +
+                                     str(pins[3]) + " \n")
+
+        return pins
+
+
+if __name__ == '__main__':
+    i2c_line = I2C(0)
+    i2c_line.set_addr(DEV_ADDRESS)
+    i2c_line.set_timeout(50)
+    ads1115 = ads1115_control_client(i2c_line)
+    ads1115.setup()
+    while True:
+        in_vol = read_pins_voltage(self)
+        print "Pin 0 voltage = " + str(in_vol[0])
+        print "Pin 1 voltage = " + str(in_vol[1])
+        print "Pin 2 voltage = " + str(in_vol[2])
+        print "Pin 3 voltage = " + str(in_vol[3])
+
+# if __name__ == '__main__':
+#    c = 6.144 / 32768.0
+#    i2c_line = I2C(0)
+#    i2c_line.set_addr(0x48)
+#    i2c_line.set_timeout(50)
+#    i2c_line.write([0x01,0x40,0x83])
+#    while True:
+#        i2c_line.write([0x01,0xC0,0x83])
+#        #time.sleep(0,008)
+#        while True:
+#            time.sleep(0.5)
+#	    i2c_line.write([0x01])
+#            a = i2c_line.read(2)
+#            print a[0]
+#            print a[1]
+#	    if (a[0]< 128):
+#            i2c_line.write([0x00])
+#            b = i2c_line.read(2)
+#            print b
+#            print c
+#            t =  ((b[0] << 8) + b[1])
+#            if (t >= 32768):
+#                res = t - 65535
+#            else:
+#                res = t
+#            res = res * c
+#            print res
+#            break
---
+++ Kotyarin_brain/tsl2561.py
@@ -0,0 +1,223 @@
+# This program works only for type = 0x00 and integration time = 402 ms.
+from i2cdev import *
+import time
+
+
+DEV_ADDRESS = 0x39
+# depends on ADDR sel level:
+# GND - 0x29
+# Float - 0x39
+# VDD - 0x49
+T_INT = 2
+I_GAIN = 0x10
+# 0x00: x1
+# 0x10: x16
+I_TYPE = 0x00
+# 0x00: T, FN, CL type
+# 0x01: CS type
+
+# Registers:
+REG_CONTROL = 0x00
+REG_TIMING = 0x01
+REG_THRESH_LOW_LOW = 0x02
+REG_THRESH_LOW_HIGH = 0x03
+REG_THRESH_HIGH_LOW = 0x04
+REG_THRESH_HIGH_HIGH = 0x05
+REG_INTERRUPT = 0x06
+REG_CRC = 0x08
+REG_ID = 0x0A
+REG_DATA_0_LOW = 0x0C
+REG_DATA_0_HIGH = 0x0D
+REG_DATA_1_LOW = 0x0E
+REG_DATA_1_HIGH = 0x0F
+
+CMD_TEMPLATE = 0x80
+
+RATIO_SCALE = 9
+LUX_SCALE = 14
+CH_SCALE = 10
+CHSCALE_TINT0 = 0x7517
+CHSCALE_TINT1 = 0x0fe7
+
+# Only for i_type = 0x00
+K1C = 0x0043
+B1C = 0x0204
+M1C = 0x01ad
+K2C = 0x0085
+B2C = 0x0228
+M2C = 0x02c1
+K3C = 0x00c8
+B3C = 0x0253
+M3C = 0x0363
+K4C = 0x010a
+B4C = 0x0282
+M4C = 0x03df
+K5C = 0x014d
+B5C = 0x0177
+M5C = 0x01dd
+K6C = 0x019a
+B6C = 0x0101
+M6C = 0x0127
+K7C = 0x029a
+B7C = 0x0037
+M7C = 0x002b
+K8C = 0x029a
+B8C = 0x0000
+M8C = 0x0000
+K1T = 0x0040
+B1T = 0x01f2
+M1T = 0x01be
+K2T = 0x0080
+B2T = 0x0214
+M2T = 0x02d1
+K3T = 0x00c0
+B3T = 0x023f
+M3T = 0x037b
+K4T = 0x0100
+B4T = 0x0270
+M4T = 0x03fe
+K5T = 0x0138
+B5T = 0x016f
+M5T = 0x01fc
+K6T = 0x019a
+B6T = 0x00d2
+M6T = 0x00fb
+K7T = 0x029a
+B7T = 0x0018
+M7T = 0x0012
+K8T = 0x029a
+B8T = 0x0000
+M8T = 0x0000
+
+
+class Tsl2561_control_client ():
+    def __init__(self, i2c_, i_type=I_TYPE,
+                 i_gain=I_GAIN, address=DEV_ADDRESS):
+        self.gain = i_gain
+        self.ch_scale = (1 << CH_SCALE)
+
+        if (not self.gain):
+            self.ch_scale = self.ch_scale << 4
+
+        self.type = i_type
+        self.addr = address
+        self.i2c_line = i2c_
+        self.i2c_addr = address
+        self.data_1 = -1
+        self.data_0 = -1
+
+    def setup(self):
+        self.i2c_line.set_addr(self.i2c_addr)
+
+        self.telemetry_log_lux = open('telemetry_log/lux.txt', 'a')
+        self.telemetry_log_lux.write("Time,LUX\n")
+
+        self._write_reg(REG_CONTROL, 0x03)
+        self._write_reg(REG_TIMING, 0x02 | self.gain)
+        self._write_reg(REG_INTERRUPT, 0x00)
+
+    def _write_reg(self, reg, val):
+        data = [CMD_TEMPLATE | reg]
+        data.append(val)
+        i2c_line.write(data)
+
+    def _read_raw_lux(self):
+        self.i2c_line.set_addr(self.i2c_addr)
+
+        self.i2c_line.write([0x80 | 0x20 | 0x0E])
+        buf = self.i2c_line.read(2)
+        self.data_1 = (buf[1] << 8) | buf[0]
+
+        self.i2c_line.write([0x80 | 0x20 | 0x0C])
+        buf = self.i2c_line.read(2)
+        self.data_0 = (buf[1] << 8) | buf[0]
+
+    def get_lux(self):
+        self._read_raw_lux()
+
+        channel_0 = (self.data_0 * self.ch_scale) >> CH_SCALE
+        channel_1 = (self.data_1 * self.ch_scale) >> CH_SCALE
+
+        ratio1 = 0
+        if (channel_0 != 0):
+            ratio1 = (channel_1 << (RATIO_SCALE + 1)) / channel_0
+
+        ratio = (ratio1 + 1) >> 1
+
+        if (self.type == 0x00):
+            if ((ratio >= 0) and (ratio <= K1T)):
+                b = B1T
+                m = M1T
+            elif (ratio <= K2T):
+                b = B2T
+                m = M2T
+            elif (ratio <= K3T):
+                b = B3T
+                m = M3T
+            elif (ratio <= K4T):
+                b = B4T
+                m = M4T
+            elif (ratio <= K5T):
+                b = B5T
+                m = M5T
+            elif (ratio <= K6T):
+                b = B6T
+                m = M6T
+            elif (ratio <= K7T):
+                b = B7T
+                m = M7T
+            elif (ratio > K8T):
+                b = B8T
+                m = M8T
+
+        elif (self.type == 0x01):
+            if ((ratio >= 0) and (ratio <= K1C)):
+                b = B1C
+                m = M1C
+            elif (ratio <= K2C):
+                b = B2C
+                m = M2C
+            elif (ratio <= K3C):
+                b = B3C
+                m = M3C
+            elif (ratio <= K4C):
+                b = B4C
+                m = M4C
+            elif (ratio <= K5C):
+                b = B5C
+                m = M5C
+            elif (ratio <= K6C):
+                b = B6C
+                m = M6C
+            elif (ratio <= K7C):
+                b = B7C
+                m = M7C
+            elif (ratio > K8C):
+                b = B8C
+                m = M8C
+
+        temp = (channel_0 * b) - (channel_1 * m)
+
+        if (temp < 0):
+            temp = 0
+
+        temp += (1 << (LUX_SCALE - 1))
+        lux = temp >> LUX_SCALE
+        self.telemetry_log_lux.write(str(time.time()) + "," +
+                                     str(lux) + " \n")
+        return lux
+
+    def close(self):
+        self.telemetry_log_lux.close()
+
+
+if __name__ == '__main__':
+    i2c_line = I2C(0)
+    i2c_line.set_addr(DEV_ADDRESS)
+    i2c_line.set_timeout(50)
+    tsl2561 = tsl2561_control_client(i2c_line)
+    tsl2561.setup()
+    while True:
+        lux = tsl2561.get_lux()
+        print lux
+        time.sleep(0.5)
