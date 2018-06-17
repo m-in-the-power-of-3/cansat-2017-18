ae8244cf609413107c4b9762e91a795d1ace0565
Agnus <tenebris.agnus@yandex.ru>
2018-06-17 20:02:04 +0300

    Начала писать main

--- Kotyarin_brain/main.py
+++ Kotyarin_brain/main.py
@@ -1,38 +1,79 @@
-import logging
-import serial
-import traceback
-import sys
-
-from librepilot.uavtalk.uavobject import *
-from librepilot.uavtalk.uavtalk import *
-from librepilot.uavtalk.objectManager import *
-from librepilot.uavtalk.connectionManager import *
+#import serial
+import i2cdev
 
 import revolution
+import ads1115
+import tsl2561
 
+# UAV talk
+PORT_REVOLUTION = "/dev/ttyS1"
+
+# UART
+PORT_UART = "/dev/ttyS2"
+
+# I2C
+PORT_I2C = 0
+I2C_TIMEOUT = 2
+
+# Revolution
+REV_LOG_UPDATE_RATE = 50
+
+REV_SERVO_MIN = 700
+REV_SERVO_MAX = 2500
+REV_SERVO_DEG = 180
+REV_SERVO_CHANNEL = 0
+REV_SERVO_ROT_1 = 500
+REV_SERVO_ROT_2 = 1000
+REV_SERVO_ROT_3 = 1500
 
 
 if __name__ == '__main__':
+    # ================================================================
+    # Init block
+    # ================================================================
+    # UAV talk
+    uav_talk = revolution.Uavtalk()
+    uav_talk.setup(PORT_REVOLUTION)
 
-    if len(sys.argv) != 2:
-        print "ERROR: Incorrect number of arguments"
-        sys.exit(2)
+    # Uart
+    # -------------------------------------------------------------<==
+    # TODO: init uart for GSM
+    # -------------------------------------------------------------<==
 
-    port = sys.argv[1]
+    # I2C
+    i2c_line = i2cdev.I2C(PORT_I2C)
+    i2c_line.set_timeout(I2C_TIMEOUT)
 
-    UavT = revolution.Uavtalk()
-    UavT.setup(port)
+    # Revolution - log
+    revolution_log = revolution.All_telemetry_logger(uav_talk,
+                                                     REV_LOG_UPDATE_RATE)
+    revolution_log.setup_all()
 
-    Telemetry_log = revolution.All_telemetry_logger(UavT,50)
-    Telemetry_log.setup_all()
+    # Revolution - servo
+    revolution_servo = revolution.Servo_control_client(uav_talk,
+                                                       REV_SERVO_CHANNEL,
+                                                       REV_SERVO_MIN,
+                                                       REV_SERVO_MAX,
+                                                       REV_SERVO_DEG,)
 
-    Servo = revolution.Servo_control_client(UavT,0,1000,2000,180)
+    # Revolution - buzzer
+    # -------------------------------------------------------------<==
+    # TODO: write module for buzzer
+    # -------------------------------------------------------------<==
 
-    while True:
-        try:
-            a = int(input())
-        except:
-            a = -1
-        if (a >= 0):
-            print "Rotation"
-            Servo.rotation(a)
+    # GSM
+    # -------------------------------------------------------------<==
+    # TODO: write module for GSM
+    # -------------------------------------------------------------<==
+
+    # TSL2561
+    pi_tsl2561 = tsl2561.Tsl2561_control_client()
+    pi_tsl2561.setup()
+
+    # Pi - ADS1115
+    pi_ads1115 = ads1115.Ads1115_control_client(i2c_line)
+    ads1115.setup()
+
+    # ================================================================
+    # First data block
+    # ================================================================
