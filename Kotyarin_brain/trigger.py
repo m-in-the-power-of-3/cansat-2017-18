b6f5b4d0ea598c593525af26e8704250ce83a14f
Agnus <tenebris.agnus@yandex.ru>
2018-06-28 17:30:28 +0300

    добавила модуль для переключалки

---
+++ Kotyarin_brain/trigger.py
@@ -0,0 +1,24 @@
+import RPi.GPIO as GPIO
+import time
+
+FUSE_PIN = 24
+
+
+class Trigger_control_client ():
+    def __init__(self, pin_=FUSE_PIN):
+        self.pin = pin_
+
+    def setup(self):
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(self.pin, GPIO.IN)
+
+    def read(self):
+        return GPIO.input(self.pin)
+
+
+if __name__ == '__main__':
+    trigger = Trigger_control_client()
+    trigger.setup()
+    while True:
+        print trigger.read()
+        time.sleep(0.1)
